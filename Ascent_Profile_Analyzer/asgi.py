3cd00254ff33865dfe37f02cc9944b399302d2fb
Devin Hedge <devin@devinhedge.com>
2020-01-25 21:35:16 -0500

    checking in django files
    checking in django files

---
+++ Ascent_Profile_Analyzer/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for Ascent_Profile_Analyzer project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/3.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Ascent_Profile_Analyzer.settings')
+
+application = get_asgi_application()
